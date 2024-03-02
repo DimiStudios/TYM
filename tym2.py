39efda9c20935012e44fd9bac316d77889325cfd
DIMIBAGA <dianabarrantesga1510@gmail.com>
2024-03-02 10:00:15 -0500

    x

--- tym2.py
+++ tym2.py
@@ -9,13 +9,36 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
-playlist = ["Manda una señal", 
+playlist = [
+            "Manda una señal", 
             "De pies a cabeza", 
             "Corazón espinado", 
             "Ángel de amor", 
             "How can it be now?", 
             "Labios compartidos",
-            "Knee Socks"]
+            "Knee Socks"
+            "Santana - La Flaca ft. Juanes"
+            ]
+
+maná = [
+        "Manda una señal", 
+        "De pies a cabeza",
+        "Corazón espinado",
+        "Ángel de amor",
+        "Labios compartidos",
+        "¿Dónde jugarán los niños?",
+        "Vivir sin aire",
+        "Bendita tu luz",
+        "Mariposa Traicionera"
+        "Se me olvidó otra vez"
+        "El rey maná"
+        "Te lloré un río"
+        "Huele a tristeza"
+        "Eres mi religión"
+        "Me vale"
+        "En el muelle de san blas"
+        "Clavado en un bar"
+        "Rayando el sol"]
 
 def talk(text):
     engine.say(text)
@@ -50,6 +73,12 @@ def run_tym():
             elif "pon cualquier cosa" in rec:
                 if len(playlist) > 0:
                     random_song = random.choice(playlist)
+                    print("Reproduciendo " + random_song)
+                    talk("Reproduciendo " + random_song)
+                    pywhatkit.playonyt(random_song)
+            elif "maná" in rec:
+                if len(maná) > 0:
+                    random_song = random.choice(maná)
                     print("Reproduciendo " + random_song)
                     talk("Reproduciendo " + random_song)
                     pywhatkit.playonyt(random_song)
